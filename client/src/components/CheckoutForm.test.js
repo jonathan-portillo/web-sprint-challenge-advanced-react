c350f037076040c9d06c570931ad5ff6b4b06432
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-09-13 22:04:57 -0400

    hooks doneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eee eeeeeee eee eee ee e ee

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,7 +4,7 @@ import CheckoutForm from "./CheckoutForm";
 import { text } from "body-parser";
 
 // Write up the two tests here and make sure they are testing what the title shows
-text();
+
 test("form header renders", () => {});
 
 test("form shows success message on submit with form details", () => {});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
-// write your custom hook here to control your checkout form
+import React, { useState } from "react";
+
+const useForm = (intialValue, successMessage) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(successMessage);
+  const [values, setValues] = useState(intialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+  return [showSuccessMessage, values, handleChanges, handleSubmit];
+};
+
+export default useForm;

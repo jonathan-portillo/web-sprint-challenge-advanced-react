198f6de981a2399a046f72e24852bfcef3b69640
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-09-13 21:13:23 -0400

    started

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { text } from "body-parser";
 
 // Write up the two tests here and make sure they are testing what the title shows
-
+text();
 test("form header renders", () => {});
 
 test("form shows success message on submit with form details", () => {});
